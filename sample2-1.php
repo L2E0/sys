4e190e867a54fbd2635ab5bde08ca95d884dc2a4
L2E0 <L2E0@gmail.com>
2017-06-29 10:32:47 +0900

    initial commit

---
+++ sample2-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="./../css/dokaben-css-master/dokaben.css">
+        <link rel="stylesheet" href="./../css/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <meta http-equiv="Content-Type" contet="text/html;charset=UTF-8" />
+        <title>Bootstrap dokaben test</title>
+    </head>
+    <body>
+
+        <p class="dokaben dkbn-loop dkbn-text" style="font-size: 2em;">こんにちは。</p>
+        <div class="btn-group btn-group-lg" role="group" aria-label="...">...</div>
+        <button type="button" class="btn btn-default" aria-label="Left Align">
+            <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+        </button>
+        <br>
+        <a href="./select_thread.php" class="dkbn-hover"><span class="dokaben dkbn-loop2 dkbn-text" style="font-size: 2em;">さようなら</span></a>
+
+    </body>
+</html>
---
+++ sample2-2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="./../css/dokaben-css-master/dokaben.css">
+<link rel="stylesheet" href="./../css/bootstrap-3.3.7-dist/bootstrap.css">
+<meta http-equiv="Content-Type" contet="text/html;charset=UTF-8" />
+<title>サンプル</title>
+</head>
+<body>
+
+<p class="dokaben dkbn-loop dkbn-text" style="font-size: 5em;">こんにちは</p>
+<br>
