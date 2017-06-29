09fdf45553b35d36d6d40ef98fb6c21e391c29d0
l2e0 <l2e09890@gmail.com>
2017-06-29 11:54:39 +0900

    add sample

--- sample2-1.php
+++ sample2-1.php
@@ -1,17 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <link rel="stylesheet" href="./../css/dokaben-css-master/dokaben.css">
-        <link rel="stylesheet" href="./../css/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href=".\css\dokaben-css-master\dokaben.css">
+        <link rel="stylesheet" href=".\css\bootstrap-3.3.7-dist\css\bootstrap.min.css">
         <meta http-equiv="Content-Type" contet="text/html;charset=UTF-8" />
         <title>Bootstrap dokaben test</title>
     </head>
     <body>
+        <CENTER>
+            <p align="center" class="dokaben dkbn-loop dkbn-text" style="font-size: 5em;">Sample</p>
+            <br>
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <button  class="btn btn-danger" type="button" style="float: left;">Go</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </CENTER>
 
         <p class="dokaben dkbn-loop dkbn-text" style="font-size: 2em;">こんにちは。</p>
         <div class="btn-group btn-group-lg" role="group" aria-label="...">...</div>
         <button type="button" class="btn btn-default" aria-label="Left Align">
-            <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></span>
         </button>
         <br>
         <a href="./select_thread.php" class="dkbn-hover"><span class="dokaben dkbn-loop2 dkbn-text" style="font-size: 2em;">さようなら</span></a>
