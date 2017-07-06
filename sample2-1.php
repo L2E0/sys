dc49b695abed020e7289f5ac3bbb4de3aab14b59
l2e0 <l2e09890@gmail.com>
2017-07-06 11:57:39 +0900

    modify sample2-1

--- sample2-1.php
+++ sample2-1.php
@@ -8,28 +8,48 @@
     </head>
     <body>
         <CENTER>
-            <p align="center" class="dokaben dkbn-loop dkbn-text" style="font-size: 5em;">Sample</p>
-            <br>
-
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search for...">
-                        <span class="input-group-btn">
-                            <button  class="btn btn-danger" type="button" style="float: left;">Go</button>
-                        </span>
-                    </div>
-                </div>
-            </div>
+            <p align="center" class="dokaben dkbn-loop dkbn-text" style="font-size: 5em;">は？</p>
         </CENTER>
 
+        <div style="text-align: right;">
+            <div class="btn-group" style="margin-left: auto; margin-right: 0;">
+                <button type="button" class="btn btn-default">Action</button>
+                <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="caret"></span>
+                    <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu" style="position: static;">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">あ</a></li>
+                    <li><a href="#">い</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                        <button  class="btn btn-danger" type="button" style="float: left;">は？</button>
+                    </span>
+                </div>
+            </div>
+        </div>
+
         <p class="dokaben dkbn-loop dkbn-text" style="font-size: 2em;">こんにちは。</p>
-        <div class="btn-group btn-group-lg" role="group" aria-label="...">...</div>
         <button type="button" class="btn btn-default" aria-label="Left Align">
             <span class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></span>
         </button>
         <br>
         <a href="./select_thread.php" class="dkbn-hover"><span class="dokaben dkbn-loop2 dkbn-text" style="font-size: 2em;">さようなら</span></a>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+        </script>
+        <script src=".\css\bootstrap-3.3.7-dist\js\bootstrap.min.js"></script>
 
     </body>
 </html>
