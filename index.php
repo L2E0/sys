686c49a5e001b90b9be3a298d4907cb7b194113a
l2e0 <l2e09890@gmail.com>
2017-08-07 17:00:22 +0900

    index.php alpha

--- index.php
+++ index.php
@@ -1,140 +1,244 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href=".\css\dokaben-css-master\dokaben.css">
+        <link rel="stylesheet" href=".\css\bootstrap-3.3.7-dist\css\bootstrap.min.css">
+        <link href='http://fonts.googleapis.com/css?family=Coming+Soon' rel='stylesheet' type='text/css'>
+        <meta charset=utf-8>
+        <title>Bootstrap dokaben test</title>
+        <style>
+            .dekaben {
+                        display: inline-block;
+                        font-size: 3em;
+                        text-align: center;
+                    }
+                    .parent{
+                        position: relative;
+                        height: 200px;
+                        width: 300px;
+                    }
+                    .inner{
+                        top: 0;
+                        bottom: 0;
+                        left: 0;
+                        right: 0;
+                        position: absolute;
+                        margin: auto;
+                    }
+        </style>
+    </head>
+    <body>
+        <a type="button" class="btn btn-default" style="margin-left:20px;" href="index.php">
+            <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+        </a>
+        <a  href="riyoukiyaku.html" type="button" class="btn btn-warning">利用規約</a>
+        <a href="shiyouhoho.html" type="button" class="btn btn-success">使用方法</a>
 
-session_start();
-$GLOBALS['dbname'] = "group1_db";
-$GLOBALS['dbuser'] = "group1";
-$GLOBALS['dbhost'] = "localhost";
+        <div style="display: inline-block; float: right; overflow: visible;">
+            <div class="btn-group">
+                <button type="button" style="width: 160px; text-align: right;"class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Action <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" style="width: 100px;">
+                    <li id="signup"><a href="./signup.php">sign up</a></li>
+                    <li id="signin"><a href="./signin.php">sign in</a></li>
+                    <li id="myp"><a href="./mypage.php">my page</a></li>
+                    <li id="crtth"><a href="./post.php">create thread</a></li>
+                    <li id="bar" role="separator" class="divider"></li>
+                    <li id="signout"><a href="./signout.php">sign out</a></li>
+                </ul>
+            </div>
+        </div>
 
-//------
 
-function dbaccess($sql, &$fields, &$data) {
-	$dbname = $GLOBALS['dbname'];
-	$dbuser = $GLOBALS['dbuser'];
-	$dbhost = $GLOBALS['dbhost'];
+        <script type="text/javascript">
+            <?php
+            ob_start();
+            session_start();
+                if (isset($_SESSION["userid"])) {//sign in
+                    echo <<< GOMI
+                    document.getElementById("signup").style.display="none";
+                    document.getElementById("signin").style.display="none";
+                    document.getElementById("myp").style.display="inline";
+                    document.getElementById("crtth").style.display="inline";
+                    document.getElementById("signout").style.display="inline";
+                    document.getElementById("bar").style.display="block";
+GOMI;
+                } else {
+                    echo <<< GOMI
+                    document.getElementById("signup").style.display="inline";
+                    document.getElementById("signin").style.display="inline";
+                    document.getElementById("myp").style.display="none";
+                    document.getElementById("crtth").style.display="none";
+                    document.getElementById("signout").style.display="none";
+                    document.getElementById("bar").style.display="none";
+GOMI;
+                }
+            ?>
+        </script>
 
-	$ans = "";
-	if ($dbhost == "" || $dbhost == "localhost") {
-		$con_str = "dbname={$dbname} user={$dbuser}";
-	} else {
-		$con_str = "dbname={$dbname} user={$dbuser} host={$dbhost}";
-	}
-	$con = pg_connect($con_str);
-	if ($con == false) {
-		$ans .= "{$php_errormsg}<br>\n";
-		return $ans;
-	}
+        <hr style="border-color: #ff0000; margin-top: 1px;">
 
-	$fields = array();
-	$data = array();
-	$result = pg_query($con, $sql);
-	if ($result == false) {
-		$ans .= pg_last_error($con) . "sql = {$sql}<br>\n";
-		return $ans;
-	}
+        <div style="text-align: center;">
+            <p class="dokaben dkbn-text dkbn-up" style="animation-duration: 10000ms; font-size: 8em; display: inline-block;">ランキングくん</p>
+        </div>
 
-	$num_rows = pg_num_rows($result);
-	$num_cols = pg_num_fields($result);
-	for ($i = 0; $i < $num_cols; $i++) {
-		$fields[$i] = pg_field_name($result, $i);
-	}
-	for ($i = 0; $i < $num_cols; $i++) {
-		for ($j = 0; $j < $num_rows; $j++) {
-			$data[$i][$j] = pg_fetch_result($result, $j, $i);
-		}
-	}
-	pg_free_result($result);
-	pg_close($con);
-	return $ans;
-}
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <a href="#"><button  class="btn btn-info" type="button" style="float: left;">検索</button></a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-2 col-sm-offset-5">
+                    <a href="select_thread.php" type="button" class="btn btn-danger">詳細検索へGo</a>
+                </div>
+            </div>
 
-//------
+            <div class="row" style="margin-top: 10px;">
+                <div class="col-md-4">
+                    <div class="panel panel-info" >
+                        <div class="panel-heading" >
+                            <p style="margin: 0px 0px 0px 30px;"><span class="dokaben dkbn-text dkbn-loop dekaben" >Genre</span></p>
+                        </div>
+                        <div class="panel-body">
+                            <ul style="list-style: none;">
+                                <a href="#?genre='a'" style="font-size: 2em;"><li>Horror</li></a>
+                                <a href="#" style="font-size: 2em;"><li>Action</li></a>
+                                <a href="#" style="font-size: 2em;"><li>Battle</li></a>
+                                <a href="#" style="font-size: 2em;"><li>Fighting</li></a>
+                                <a href="#" style="font-size: 2em;"><li>Shooting</li></a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="panel panel-info">
+                        <div class="panel-heading" >
+                            <p style="margin: 0px 0px 0px 12px;"><span class="dokaben dkbn-text dkbn-loop dekaben" >New Comments</span></p>
+                        </div>
+                        <div class="panel-body">
+                            <?php
+                                $dbname='group1_db';
+                                $dbuser='group1';
+                                $dbhost='localhost';
 
-print <<<EOT
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html lang="ja">
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=EUC-JP">
-<meta http-equiv="Pragma" content="no-cache">
-<meta http-equiv="Expires" content="Fri, 31 Dec 2004 23:59:59 GMT">
-<title></title>
-</head>
-<body>
-EOT;
+                                $con=pg_connect("dbname=group1_db user=group1");
+                                if(!$con){
+                                    die('failure' .pg_last_error());
+                                }
+                                pg_set_client_encoding('UTF-8');
+                                $sql='select * from comment order by date desc, time desc limit 5';
+                                $result=pg_query($sql);
+                                if(!$result){
+                                    die('fail to execute the query' .pg_last_error());
+                                }
 
-$mode = isset($_REQUEST["mode"])?$_REQUEST["mode"]:"";
-$request_userid = isset($_REQUEST["request_userid"])?$_REQUEST["request_userid"]:"";
-$request_name = isset($_REQUEST["request_name"])?$_REQUEST["request_name"]:"";
-$request_userid = pg_escape_string($request_userid);
-$request_name = mb_convert_encoding(pg_escape_string($request_name), "EUC-JP");
-$msg = "";
+                            ?>
+                            <style>
+                                span.title{
+                                    font-size: 27px;
+                                    color: #000f00;
+                                }
+                                span.comment{
+                                    font-size:35px;
+                                    color: #0000f0;
+                                }
+                                span.datetime{
+                                    font-size:18px;
+                                    color: #000000;
+                                }
+                                span.in{
+                                    font-size:18px;
+                                    color: #000000;
+                                }
+                            </style>
+                            <?php
+                                while($comment = pg_fetch_assoc($result)){
+                                    echo('<a href="./show_thread.php?no=' .$comment['no'] .'">' .'<span class="comment">'  .$comment['comment'] .'</span><span class="in"> in
+                                    </span>  <span class="title">' .$comment['title'] .': </span><span class="datetime">' .$comment['date'] .'-' .$comment['time'] .'</span></a><br>');
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="panel panel-info">
+                        <div class="panel-heading" >
+                            <p style="margin: 0px 0px 0px 30px;"><span class="dokaben dkbn-text dkbn-loop dekaben" >Thread Rank</span></p>
+                        </div>
+                            <?php
+                                $evaary = array_pad(array(), 5, NULL);
+                                $titleary = array_pad(array(), 5, NULL);
+                                $numary = array_pad(array(), 5, NULL);
 
-switch ($mode) {
+                                $sql='select * from thread order by evaluation desc limit 5';
+                                $result=pg_query($sql);
 
-case "register":
-	$fields = array();
-	$data = array();
-	$sql = "insert into users values('{$request_userid}', '{$request_name}')";
-	if (($ans = dbaccess($sql, $fields, $data)) != "") {
-		$msg .= "<li><font color=\"red\">database operation failed. SQL = {$sql}.</font><br>";
-		$msg .= "$ans";
-		break;
-	}
-	break;
+                                if(!$result){
+                                    die('fail to execute the query' .pg_last_error());
+                                }
+                                while($row = pg_fetch_assoc($result)){
+                                    for($i=0; $i<count($evaary); $i++){
+                                        if($evaary[$i] < $row['evaluation']){
+                                            $evaary[$i] = $row['evaluation'];
+                                            $titleary[$i] = $row['title'];
+                                            $numary[$i] = $row['no'];
+                                            break;
+                                        }
+                                    }
+                                }
+                            ?>
+                        <div class="panel-body">
 
-case "remove":
-	$fields = array();
-	$data = array();
-	$sql = "delete from users where userid='{$request_userid}'";
-	if (($ans = dbaccess($sql, $fields, $data)) != "") {
-		$msg .= "<li><font color=\"red\">database operation failed. SQL = {$sql}.</font><br>";
-		$msg .= "$ans";
-		break;
-	}
-	break;
+                            <p style="font-size: 3em;">
+                            <?php
+                                if($titleary[0] != NULL)
+                                echo('<a href="./show_thread.php?no=' .$numary[0] .'">' .$titleary[0] .' : ★' .$evaary[0] .'</a>');
+                            ?>
+                            </p>
+                            <p style="font-size: 2em;">
+                            <?php
+                                if($titleary[1] != NULL)
+                                echo('<a href="./show_thread.php?no=' .$numary[1] .'">' .$titleary[1] .' : ★' .$evaary[1] .'</a>');
+                            ?>
+                            </p>
+                            <p style="font-size: 2em;">
+                            <?php
+                                if($titleary[2] != NULL)
+                                echo('<a href="./show_thread.php?no=' .$numary[2] .'">' .$titleary[2] .' : ★' .$evaary[2] .'</a>');
+                            ?>
+                            </p>
+                            <p style="font-size: 2em;">
+                            <?php
+                                if($titleary[3] != NULL)
+                                echo('<a href="./show_thread.php?no=' .$numary[3] .'">' .$titleary[3] .' : ★' .$evaary[3] .'</a>');
+                            ?>
+                            </p>
+                            <p style="font-size: 2em;">
+                            <?php
+                                if($titleary[4] != NULL)
+                                echo('<a href="./show_thread.php?no=' .$numary[4] .'">' .$titleary[4] .' : ★' .$evaary[4] .'</a>');
+                            ?>
+                            </p>
+                            <p style="font-size: 1em;">
+                            <a href="ranking.php" class="dkbn-hover"><span class="dokaben dkbn-loop2">ランキングをさらに見る</span></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-case "":
-	break;
-}
 
-do {
-	$fields = array();
-	$data = array();
-	$sql = "select userid, name from users";
-	if (($ans = dbaccess($sql, $fields, $data)) != "") {
-		$msg .= "<li><font color=\"red\">database operation failed. SQL = {$sql}.</font><br>";
-		$msg .= "$ans";
-		break;
-	}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+        </script>
+        <script src=".\css\bootstrap-3.3.7-dist\js\bootstrap.min.js"></script>
 
-	$num_rows = count($data[0]);
-	for ($i = 0; $i < $num_rows; $i++) {
-		$userid 	= $data[0][$i];
-		$name 	= $data[1][$i];
-		$src = implode(",", array(
-			$userid,
-			$name
-		));
-		$src .= "<br>\r\n";
-		$msg .= $src;
-	}
-} while (0);
-
-print <<<EOT
-<ul>
-<li><form enctype="multipart/form-data" action={$_SERVER['PHP_SELF']} method="POST">
-userid: <input name="request_userid" type="TEXT" value="{$request_userid}">
-name: <input name="request_name" type="TEXT" value="{$request_name}">
-<input type="submit" name="mode" value="register"><br>
-</form>
-<li><form enctype="multipart/form-data" action={$_SERVER['PHP_SELF']} method="POST">
-userid: <input name="request_userid" type="TEXT" value="{$request_userid}">
-<input type="submit" name="mode" value="remove"><br>
-</form>
-$msg
-</ul>
-</body>
+    </body>
 </html>
-EOT;
-?>
-
-
